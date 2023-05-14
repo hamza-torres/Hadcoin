5dba474916ec5d5956f2e4829f4ab4fe05cddfc5
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2023-05-14 21:00:35 +0200

    working-blockchain-api

--- Blockchain/app.py
+++ Blockchain/app.py
@@ -1,41 +1,42 @@
-import Blockchain
+# API
+from blockchain import Blockchain
 from flask import Flask, jsonify
-
+# Creating a Web App
 app = Flask(__name__)
 
-# Blockchain
+# Creating a Blockchain
 blockchain = Blockchain()
 
-# Mining
-@app.route('/mineBlock', methods = ['GET'])
-def mineBlock():
-    previousBlock = blockchain.getPreviousBlock()
-    previousProof = previousBlock['proof']
-    proof = blockchain.proofOfWork(previousProof)
-    previousHash = blockchain.hash(previousBlock)
-    block = blockchain.createBlock(proof, previousHash)
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previousHash': block['previousHash']}
+                'previous_hash': block['previous_hash']}
     return jsonify(response), 200
 
-# Getting Blockchain
-@app.route('/getChain', methods = ['GET'])
-def getChain():
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
-# Checking Blockchain validity
-@app.route('/isChainValid', methods = ['GET'])
-def isChainValid():
-    isChainValid = blockchain.isChainValid(blockchain.chain)
-    if isChainValid:
-        response = {'message': 'The Blockchain is valid.'}
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
     else:
-        response = {'message': 'The Blockchain is invalid.'}
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
 # Running the app
